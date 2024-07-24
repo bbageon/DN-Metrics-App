a179f376b3f65732d34b740de2489a43e8d2430e
김건우 <bbageon@gimgeon-uui-MacBookPro.local>
2024-07-24 13:52:43 +0900

    webview setting

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WebView from 'react-native-webview';
 import Router from '../routes';
 
 export default function App() {
   return (
-    <Router/>
+    // <Router/>
+    <WebView
+      source={{ uri : 'https://dnmetrics.co.kr/'}}
+    />
   );
 }
 
